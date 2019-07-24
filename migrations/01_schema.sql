73d7082740cb636f123bda458707b82c7a61ec6d
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-24 21:15:32 +0000

    added schema file

---
+++ migrations/01_schema.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL
+  );
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER NOT NULL REFERENCES users(id),
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  thumbnail_photo_url TEXT NOT NULL,
+  cover_photo_url TEXT NOT NULL,
+  cost_per_night SMALLINT NOT NULL,
+  parking_spaces SMALLINT NOT NULL,
+  number_of_bathrooms SMALLINT NOT NULL,
+  number_of_bedrooms SMALLINT NOT NULL,
+  country TEXT NOT NULL,
+  street TEXT NOT NULL,
+  city TEXT NOT NULL,
+  province TEXT NOT NULL,
+  post_code TEXT NOT NULL,
+  active BOOLEAN NOT NULL
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER NOT NULL REFERENCES properties(id),
+  guest_id INTEGER NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER NOT NULL REFERENCES users(id),
+  reservation_id INTEGER NOT NULL REFERENCES reservations(id),
+  property_id INTEGER NOT NULL REFERENCES properties(id),
+  rating SMALLINT NOT NULL,
+  message TEXT NOT NULL
+);
