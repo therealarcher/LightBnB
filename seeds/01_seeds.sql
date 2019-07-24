d063e716e8d2d8f5f611667e5246e2ac82c4f66e
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-24 21:38:00 +0000

    added some fake data to seed file

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(name, email, password)
+VALUES ('Sweets Maleet', 'sweets_maleet@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Scoots McDougall', 'scoots@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
