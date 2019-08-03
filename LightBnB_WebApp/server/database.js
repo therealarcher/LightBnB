c4bc81232b5c201ab20a11c5a7369c37af11e0fd
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-08-03 20:37:53 +0000

    allows user login

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -81,6 +81,7 @@ const addUser = function(user) {
   return pool.query(`
     INSERT INTO users (name, email, password)
     VALUES ($1, $2, $3)
+    RETURNING *
     `, [user.name, user.email, user.password])
     .then(res => res.rows[0])
     .catch((error) => console.error(error));
